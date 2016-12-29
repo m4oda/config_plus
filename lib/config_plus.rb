538ca53f0e2b0a9345e48e388c8c0f65fa83d6dc
m4oda <e5ww2sze@gmail.com>
2016-12-30 03:57:21 +0900

    use autoload

--- lib/config_plus.rb
+++ lib/config_plus.rb
@@ -1,8 +1,11 @@
 require 'config_plus/base'
-require 'config_plus/config'
-require 'config_plus/default_loader_logic'
-require 'config_plus/helper'
-require 'config_plus/loader'
-require 'config_plus/merger'
-require 'config_plus/node'
 require 'config_plus/version'
+
+module ConfigPlus
+  autoload :Config, 'config_plus/config'
+  autoload :DefaultLoaderLogic, 'config_plus/default_loader_logic'
+  autoload :Helper, 'config_plus/helper'
+  autoload :Loader, 'config_plus/loader'
+  autoload :Merger, 'config_plus/merger'
+  autoload :Node, 'config_plus/node'
+end
