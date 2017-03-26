500b32557b585096b8bf3ea88ca5fa26a5c05c2f
m4oda <e5ww2sze@gmail.com>
2017-03-26 17:03:31 +0900

    simplify Config class a little

--- lib/config_plus/config.rb
+++ lib/config_plus/config.rb
@@ -1,43 +1,34 @@
 module ConfigPlus
+  # Configuration of ConfigPlus
   class Config
-    class << self
-      attr_reader :properties
-
-      def prop_accessor(*names)
-        @properties ||= []
-        @properties.concat(names)
-        attr_accessor *names
-      end
-
-      private :prop_accessor
+    def self.default_properties
+      {
+        config_method: :config,
+        extension: nil,
+        namespace: nil,
+        node_model: Node,
+        root_dir: nil,
+        source: nil,
+        loader_logic: :default,
+      }
     end
-
-    prop_accessor :config_method,
-                  :extension,
-                  :namespace,
-                  :node_model,
-                  :root_dir,
-                  :source,
-                  :loader_logic
 
     def initialize
-      @config_method = :config
-      @extension = nil
-      @loader_logic = :default
-      @namespace = nil
-      @node_model = Node
-      @root_dir = nil
-      @source = nil
+      @properties = self.class.default_properties
+      setup_attrs
     end
 
+    # returns a new loader instance
     def loader
       Loader.new(self)
     end
 
+    # returns loader class specified by +loader_logic+ property
     def loader_logic
-      return @loader_logic if @loader_logic.is_a?(Class)
+      setting = properties[:loader_logic]
+      return setting if setting.is_a?(Class)
 
-      name = ::ConfigPlus::Helper.classify(@loader_logic.to_s)
+      name = ::ConfigPlus::Helper.classify(setting.to_s)
       name = "#{name}LoaderLogic"
       raise "Unknown loader logic named `#{name}'" unless
         ::ConfigPlus::const_defined?(name)
@@ -49,11 +40,24 @@ module ConfigPlus
     end
 
     def has_property?(name)
-      self.class.properties.include?(name.to_sym)
+      properties.keys.include?(name.to_sym)
     end
 
     def property_set(name, value)
-      instance_variable_set("@#{name}", value)
+      properties[name.to_sym] = value
+    end
+
+    private
+
+    attr_reader :properties
+
+    def setup_attrs
+      singleton_class.instance_exec(methods, properties) do |used, props|
+        props.keys.each do |nm|
+          define_method "#{nm}=", ->(val) { property_set(nm, val) }
+          define_method nm, -> { properties[nm] } unless used.include?(nm)
+        end
+      end
     end
   end
 end
--- spec/config_plus/config_spec.rb
+++ spec/config_plus/config_spec.rb
@@ -1,35 +1,57 @@
 require 'spec_helper'
 
+RSpec.describe ConfigPlus::Config, '#loader_logic' do
+  let(:instance) { described_class.new }
+
+  context "when property `loader_logic' is set :default" do
+    before { instance.loader_logic = :default }
+
+    it 'returns ConfigPlus::DefaultLoaderLogic' do
+      actual = instance.loader_logic
+      expect(actual).to be ConfigPlus::DefaultLoaderLogic
+    end
+  end
+
+  context "when property `loader_logic' is set :erb_yaml" do
+    before { instance.loader_logic = :erb_yaml }
+
+    it 'returns ConfigPlus::ErbYamlLoaderLogic' do
+      actual = instance.loader_logic
+      expect(actual).to be ConfigPlus::ErbYamlLoaderLogic
+    end
+  end
+end
+
 RSpec.describe ConfigPlus::Config, '#has_property?' do
-  context "define `foo' as property with prop_accessor method" do
+  context "when an instance has property `foo'" do
     before do
-      described_class.instance_eval { prop_accessor :foo }
+      allow(described_class).to receive(:default_properties).and_return(foo: nil)
     end
 
     let(:instance) { described_class.new }
 
-    context "call with a parameter :foo" do
+    context "called with a parameter :foo" do
       it 'returns true' do
         actual = instance.has_property?(:foo)
         expect(actual).to be_truthy
       end
     end
 
-    context "call with a parameter 'foo'" do
+    context "called with a parameter 'foo'" do
       it 'returns true' do
         actual = instance.has_property?("foo")
         expect(actual).to be_truthy
       end
     end
 
-    context "call with a parameter :baa" do
+    context "called with a parameter :baa" do
       it 'returns false' do
         actual = instance.has_property?(:baa)
         expect(actual).to be_falsy
       end
     end
 
-    context "call with a parameter 'baa'" do
+    context "called with a parameter 'baa'" do
       it 'returns false' do
         actual = instance.has_property?("baa")
         expect(actual).to be_falsy
